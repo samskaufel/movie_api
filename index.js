02324611d328332aee406e8bf2b3bb11b948144f
samskaufel <85969886+samskaufel@users.noreply.github.com>
2021-12-26 12:02:00 -0800

    rearranged code, deleted bodyParser and methodOverride

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // imports express module
 // declares a variable that encapsulates Express's functionality to configure your web server
-const express = require('express');
+const express = require('express'),
+    morgan=require('morgan');
+
 const app = express();
 
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// const methodOverride = require('method-override');
+app.use(morgan('common'));
 
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
 
-// app.use(morgan('common'));
-// app.use(bodyParser.urlencoded({
-//     extended: true }));
-// app.use(bodyParser.json());
-// app.use(methodOverride());
-
-
+app.use(myLogger);
 
 let topMovies = [
     {
@@ -56,33 +54,17 @@ app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
 });
 
-app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html', { root: __dirname });
-});
-
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
+app.use(express.static('public'));
 
 
-// let myLogger = (req, res, next) => {
-//     console.log(req.url);
-//     next();
-// };
-
-// app.use(myLogger);
-
-
-
-// app.use(express.static('public'));
-
-
-
-// app.use((err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).send('Something went wrong!');
-// });
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 // listen for requests
 app.listen(8080, () => {
