38eedca9b27d7803943fc31bd413d87ec273347d
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-01-21 08:49:10 -0800

    uncommented the authentification logic in /movies endpoint

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 });
 
 // Get all movies
-app.get('/movies', /*passport.authenticate('jwt', { session: false }),*/ (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
